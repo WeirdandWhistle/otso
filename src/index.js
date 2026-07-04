f6c32aaba1e57eb46eb8feb5337ddefafb5df268
whynotjava <me@whynotjava.net>
2026-07-04 19:09:48 -0400

    google

--- src/index.js
+++ src/index.js
@@ -17,14 +17,23 @@ export default {
 		const redirect_uri = "http://localhost:8787/callback";
 		console.log("not real: ", await env.OAUTH_STATE.get("1234!"));
 
-		if(pathname == "/oauth/github"){
-			const forward_url = new URL("https://github.com/login/oauth/authorize");
+		if(pathname.startsWith("/oauth/")){
+			let forward_url;
 			const query = new URLSearchParams();
 			const response_type = "code"; query.set("response_type", response_type);
-			const client_id = env.GITUHB_CLIENT_ID; query.set("client_id", client_id);
 			query.set("redirect_uri", redirect_uri);
 			const state = generateRandomString(32); query.set("state", state );
-			await env.OAUTH_STATE.put(state, "1", {expirationTtl: 60});
+
+			if(pathname.endsWith("github")){
+				forward_url = new URL("https://github.com/login/oauth/authorize");
+				const client_id = env.GITUHB_CLIENT_ID; query.set("client_id", client_id);
+				await env.OAUTH_STATE.put(state, JSON.stringify({auth:"github"}), {expirationTtl: 60});
+			} else if(pathname.endsWith("google")){
+				forward_url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+				const client_id = env.GOOGLE_CLIENT_ID; query.set("client_id", client_id);
+				query.set("scope", "email profile");
+				await env.OAUTH_STATE.put(state, JSON.stringify({auth:"google"}), {expirationTtl: 60});
+			}			
 
 			forward_url.search = query.toString();
 			return new Response("",{
@@ -35,25 +44,37 @@ export default {
 			});
 		} else if(pathname == "/callback"){
 			const githubTokenEndpoint = "https://github.com/login/oauth/access_token";
+			const googleTokenEndpoint = "https://oauth2.googleapis.com/token";
 			const query = new URL(request.url).searchParams;
 			const code = query.get("code");
 			const state = query.get("state");
 			const grant_type = "authorization_code";
 
-			if(!await env.OAUTH_STATE.get(state))
+			const KVstateTemp = await env.OAUTH_STATE.get(state);
+			if(!KVstateTemp)
 				return new Response("Invalid State", {status: 400});
+			const KVstate = JSON.parse(KVstateTemp);
 			
 			const body = new URLSearchParams();
 			body.set("grant_type", grant_type);
 			body.set("code", code);
 			body.set("redirect_uri", redirect_uri);
-			body.set("client_id", env.GITHUB_CLIENT_ID);
-			body.set("client_secret", env.GITHUB_CLIENT_SECRET);
+			
+			let endpoint;
 
-			const res = await fetch(githubTokenEndpoint, {
+			if(KVstate.auth == "github"){
+				body.set("client_id", env.GITHUB_CLIENT_ID);
+				body.set("client_secret", env.GITHUB_CLIENT_SECRET);
+				endpoint = githubTokenEndpoint;
+			} else if(KVstate.auth == "google"){
+				body.set("client_id", env.GOOGLE_CLIENT_ID);
+				body.set("client_secret", env.GOOGLE_CLIENT_SECRET);
+				endpoint = googleTokenEndpoint;
+			}
+			
+			const res = await fetch(googleTokenEndpoint, {
 				method: 'POST',
 				headers: {
-					// 'Authorization' : `Basic ${env.CLIENT_SECRET}`,
 					'Content-Type' : 'application/x-www-form-urlencoded',
 				},
 				body: body.toString(),
@@ -79,18 +100,17 @@ export default {
 				default:
 					return new Response("Could not detect a usable format for token exchange");
 			}
-
-			const emailArray = await listGithubUserEmails(tokens.access_token);
-			let primaryEmail;
-			for(const { primary, email } of emailArray){
-				if(primary){
-					primaryEmail = email;
-					break;
-				}
+			
+			if(KVstate.auth == "github"){
+				const primaryEmail = getGithubUserEmail(tokens.access_token);
+				const username = await getGithubUsername(tokens.access_token);
+				console.log("primary email", primaryEmail);
+				console.log("username", username);
+			} else if(KVstate.auth == "google"){
+				const email = await getGoogleEmail(tokens.access_token);
+				console.log("google email", email);
 			}
-
-			console.log("primary email", primaryEmail);
-			console.log("username",await getGithubUsername(tokens.access_token));
+			
 
 			return new Response(JSON.stringify(tokens),{
 				headers:{
@@ -108,7 +128,7 @@ export default {
 	}		
 };
 
-async function listGithubUserEmails(access_token){
+async function getGithubUserEmail(access_token){
 	const res = await fetch("https://api.github.com/user/emails", {
 		headers: {
 			'Authorization' : `Bearer ${access_token}`,
@@ -120,7 +140,12 @@ async function listGithubUserEmails(access_token){
 		throw new Error("Github API failed while trying to fetch Email. Text: " + await res.text());
 	}
 
-	return await res.json();
+	const emailArray = await res.json();
+	for(const { primary, email } of emailArray){
+		if(primary){
+			return email;
+		}
+	}
 }
 
 async function getGithubUsername(access_token){
@@ -136,6 +161,21 @@ async function getGithubUsername(access_token){
 	const json = await res.json();
 	console.log(json);
 	return json.login;
+}
+
+async function getGoogleEmail(access_token){
+	const res = await fetch("https://www.googleapis.com/auth/userinfo.profile", {
+		headers: {
+			'Authorization' : `Bearer ${access_token}`,
+			'User-Agent' : userAgent
+		}
+	});
+
+	if(!res.ok){
+		throw new Error("Google API failed while trying to fetch Email. Text: " + await res.text());
+	}
+
+	return await res.text();
 }
 
 const generateRandomString = (length) => {
