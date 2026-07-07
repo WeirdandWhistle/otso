7da7c4df926088c8c502bd9206e7c15a9ea37443
whynotjava <me@whynotjava.net>
2026-07-07 14:11:41 -0400

    token, sessions, db Interationc

--- src/databaseInteraction.js
+++ src/databaseInteraction.js
@@ -21,10 +21,15 @@ export async function createUser(env, userID, username, email, issuer, id, issue
         .prepare(`
             INSERT INTO Users (userID, authenticationMethods, email, username)
                 VALUES (?, ?, ?, ?);
+            `)
+        .bind(userID, issuer, email, username) // id, issuer, issuerUsername, issuerEmail, access_token, refresh_token, userID
+        .run();
+    await env.OTSO_DB
+        .prepare(`
             INSERT INTO OAuthIssuers (ID, issuer, username, email, access_token, refresh_token, userID) 
                 VALUES (?, ?, ?, ?, ?, ?, ?);
             `)
-        .bind(userID, issuer, email, username, id, issuer, issuerUsername, issuerEmail, access_token, refresh_token, userID)
+        .bind(id, issuer, issuerUsername, issuerEmail, access_token, refresh_token, userID)
         .run();
 }
 export async function getUserIDFromSession(env, sessionID) {
@@ -35,9 +40,18 @@ export async function getUserIDFromSession(env, sessionID) {
         .bind(sessionID)
         .run());
 }
+export async function createSession(env, sessionID, userID, sessionData) {
+    await env.OTSO_DB
+        .prepare(`
+            INSERT INTO Sessions (sessionID, userID, sessionData)
+            VALUES (?, ?, ?);
+            `)
+        .bind(sessionID, userID, sessionData)
+        .run();
+}
 
 // OAuthClients
-async function getOAuthClientFromClientID(env, client_id) {
+export async function getOAuthClientFromClientID(env, client_id) {
     return returnResults(await env.OTSO_DB
         .prepare(`
             SELECT * FROM OAuthClients WHERE client_id=? LIMIT 1;
@@ -45,4 +59,14 @@ async function getOAuthClientFromClientID(env, client_id) {
         .bind(client_id)
         .run()
     );
+}
+// OAuthTokens
+export async function createOAuthToken(env, access_token, expires, scopes, refresh_token, userID, client_id) {
+    await env.OTSO_DB
+        .prepare(`
+            INSERT INTO OAuthTokens (access_token, expires, scopes, refresh_token, userID, client_id)
+            VALUES (?, ?, ?, ?, ?, ?);
+            `)
+        .bind(access_token, expires, scopes, refresh_token, userID, client_id)
+        .run()
 }
