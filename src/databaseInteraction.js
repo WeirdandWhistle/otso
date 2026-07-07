274075bef7db932a91d6d2841702aa8c88281d3c
whynotjava <me@whynotjava.net>
2026-07-06 22:51:35 -0400

    remove outdated DB tables and interation with sessions and users

--- src/databaseInteraction.js
+++ src/databaseInteraction.js
@@ -1,45 +1,20 @@
-// oauth state
-export async function oauthStatePut(env, k, v){
-    await env.OTSO_DB
-        // .prepare("INSERT INTO OAuthState (stateID, stateData) VALUES (?, ?);")
-        // .prepare(`
-        //     MERGE INTO OAuthState AS target
-        //     USING (SELECT ? AS stateID, ? AS stateData) AS source
-        //     ON target.stateID = source.stateID
-        //     WHEN MATCHED THEN
-        //         UPDATE SET stateData=source.stateData
-        //     WHEN NOT MATCHED THEN
-        //         INSERT (stateID, stateData) VALUES (?, ?);
-        //     `)
-        .prepare("INSERT OR REPLACE INTO OAuthState (stateID, stateData) VALUES (?, ?);")
-        .bind(k, v)
-        .run();
-}
-export async function oauthStateGet(env, k){
-    const res = await env.OTSO_DB
-        .prepare("SELECT stateData FROM OAuthState WHERE stateID=? LIMIT 1;")
-        .bind(k)
-        .run();
-    if(res.results.length > 0)
-        return res.results[0].stateData;
+function returnResults(raw){
+    if(raw.results.length > 0)
+        return raw.results[0];
     return null;
-}
-export async function oauthStateDelete(env, k) {
-    await env.OTSO_DB
-        .prepare("DELETE FROM OAuthState WHERE stateID=?;")
-        .bind(k)
-        .run();
 }
 // user
-export async function getUser(env, id, issuer) {
-    const { results } = await env.OTSO_DB
+export async function getUserFromIssuer(env, id, issuer) {
+    return returnResults(await env.OTSO_DB
         .prepare("SELECT * FROM users WHERE userID=(SELECT userID FROM OAuthIssuers WHERE id=? AND issuer=? LIMIT 1) LIMIT 1;")
         .bind(id, issuer)
-        .run();
-
-    if(results.length > 0)
-        return results[0];
-    return null;
+        .run());    
+}
+export async function getUserFromUserID(env, userID) {
+    return returnResults(await env.OTSO_DB
+        .prepare("SELECT * FROM users WHERE userID=? LIMIT 1;")
+        .bind(userID)
+        .run());
 }
 export async function createUser(env, userID, username, email, issuer, id, issuerUsername, issuerEmail, access_token, refresh_token){
     await env.OTSO_DB
@@ -51,4 +26,23 @@ export async function createUser(env, userID, username, email, issuer, id, issue
             `)
         .bind(userID, issuer, email, username, id, issuer, issuerUsername, issuerEmail, access_token, refresh_token, userID)
         .run();
+}
+export async function getUserIDFromSession(env, sessionID) {
+    return returnResults(await env.OTSO_DB
+        .prepare(`
+                SELECT userID FROM Sessions WHERE sessionID=? LIMIT 1;
+            `)
+        .bind(sessionID)
+        .run());
+}
+
+// OAuthClients
+async function getOAuthClientFromClientID(env, client_id) {
+    return returnResults(await env.OTSO_DB
+        .prepare(`
+            SELECT * FROM OAuthClients WHERE client_id=? LIMIT 1;
+            `)
+        .bind(client_id)
+        .run()
+    );
 }
